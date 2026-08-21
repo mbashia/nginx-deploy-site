e7518a359f9a45cbcd193f06d5230d0bd992ee7b
mbashia <mbashiavictor@gmail.com>
2026-08-21 12:38:42 +0300

    Fix scheme.

--- domain-script.sh
+++ domain-script.sh
@@ -146,6 +146,7 @@ server {
     location / {
         proxy_http_version 1.1;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
         proxy_set_header Host \$http_host;
         proxy_set_header X-Cluster-Client-Ip \$remote_addr;
         proxy_set_header Upgrade \$http_upgrade;
